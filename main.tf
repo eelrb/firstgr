2681c0b33af9da13e949d7eb3f68c88fbd266be0
eelrb <eelrb@yahoo.com>
2018-08-07 11:28:46 -0700

    no key

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAJIGB2WGX46XI7EZQ"
-  secret_key = "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"
+  access_key = "a"
+  secret_key = "b"
   region     = "us-east-1"
 }
 
