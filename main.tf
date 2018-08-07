884c005968c67df000980b08c931a1dd64903caa
eelrb <eelrb@yahoo.com>
2018-08-07 11:21:10 -0700

    create an ec2

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-"hello word"
+provider "aws" {
+  access_key = "AKIAJIGB2WGX46XI7EZQ"
+  secret_key = "mILLZi+/2+LAoPa8GYz370egGPGx4RYpBgYu5GJv"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example-test" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
